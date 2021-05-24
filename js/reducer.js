b50026c23e7c085c2de87f0369baddab6dbe7d15
jinook929 <jinook929@gmail.com>
2021-05-23 22:09:26 -0400

    commit tree 7ce2d0eb89a9b4793c68ded1eb9fbf153959ac81

--- js/reducer.js
+++ js/reducer.js
@@ -1,3 +1,5 @@
+let state = {count: 0}
+
 function changeState(state, action){
   switch (action.type) {
     case 'INCREASE_COUNT':
@@ -7,7 +9,32 @@ function changeState(state, action){
   }
 }
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
+function render() {
+  document.body.textContent = state.count
+} 
 
-changeState(state, action)
+function dispatch(action) {
+  state = changeState(state, action)
+  render()
+  // return state
+}
+
+document.addEventListener('click', e => {
+  dispatch({type: 'INCREASE_COUNT'})
+  // render()
+  console.log(e.target.children[1].textContent)
+})
+
+// let action = {type: 'INCREASE_COUNT'}
+// state = changeState(state, action)
+
+// render()
+
+// dispatch({type: 'INCREASE_COUNT'})
+// console.log(state)
+// dispatch({type: 'INCREASE_COUNT'})
+// console.log(state)
+// dispatch({type: 'INCREASE_COUNT'})
+// console.log(state)
+
+// render()
